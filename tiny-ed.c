a8863e84199de35d773c707009d14774bce4118f
Brian Durham <bkdurham@gmail.com>
2025-04-28 14:04:57 -0500

    Create tiny-ed.c

---
+++ tiny-ed.c
@@ -0,0 +1,18 @@
+/* 
+ * tiny-ed.c - Tiny Ed: A minimalist line editor
+ * 
+ * Purpose:
+ *   Recreate basic 'ed' functionality with minimal code.
+ * 
+ * Author:
+ *   Nroam Durham
+ *
+ * Created:
+ *   2025-04-28
+ *
+ * Notes:
+ *   Early development version. Only basic commands implemented.
+ *
+ * License:
+ *   [MIT License]
+ */
